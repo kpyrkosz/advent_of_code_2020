683d1c3776cd35ff23306331e5f6a54763463193
krzysztof-bsd <krzpyrkosz@gmail.com>
2020-12-15 16:12:16 +0100

    day15task1cpp

---
+++ day15/task1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+
+int main() {
+  unsigned last_spoken;
+  unsigned current_turn = 1;
+  std::unordered_map<unsigned, unsigned> val_to_last_spoken;
+  std::cin >> last_spoken;
+  while (!std::cin.eof()) {
+    val_to_last_spoken[last_spoken] = current_turn++;
+    std::cin >> last_spoken;
+    std::cin >> std::ws;
+  }
+  while (current_turn < 2020) {
+    if (val_to_last_spoken.count(last_spoken) == 0) {
+      val_to_last_spoken[last_spoken] = current_turn++;
+      last_spoken = 0;
+    } else {
+      unsigned dist = current_turn - val_to_last_spoken[last_spoken];
+      val_to_last_spoken[last_spoken] = current_turn;
+      last_spoken = dist;
+      ++current_turn;
+    }
+    std::cout << "Turn\t" << current_turn << " last spoken\t" << last_spoken
+              << '\n';
+  }
+}
